b3df97fc223d4791ec94a44e0219fa9de28fece5
손호일 <som6642@naver.com>
2023-06-08 10:13:23 +0900

    first commit

---
+++ GlobalChat/src/main/java/com/global/chat/GlobalChatApplication.java
@@ -0,0 +1,13 @@
+package com.global.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobalChatApplication.class, args);
+	}
+
+}
---
+++ GlobalChat/src/test/java/com/global/chat/GlobalChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.global.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobalChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
